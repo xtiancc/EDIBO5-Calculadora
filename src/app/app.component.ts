0b3c7879cda564964ecb31413ba9d9eb527c9a64
Cristian Carvajal <cristiancarcha@gmail.com>
2021-03-19 12:13:34 +0100

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'E5-Calculadora';
+
+  resultado: string = '';
+  calculadora: string[] = [];
+  canWrite = true;
+
+  /**
+   * 
+   * @param n
+   */
+  marcaNumero(n: string): void {
+    if (this.canWrite) {
+      this.resultado += n;
+    } else {
+      alert("Seleccione en primer lugar un operador.");
+    }
+  }
+
+  /**
+   * 
+   * @param op 
+   */
+  operacion(op: string): void {
+
+    if (this.calculadora.length === 2) {
+      this.final();
+    }
+
+    if (this.resultado != '') {
+      this.calculadora.push(this.resultado);
+      this.calculadora.push(op);
+      this.resultado = '';
+      this.canWrite = true;
+    } else {
+      alert("Indique en primer lugar un operando.");
+    }
+  }
+
+  final(): void {
+    
+    if (this.resultado != '') {
+      this.calculadora.push(this.resultado);
+
+      switch(this.calculadora[1]) {
+        case 'x': {
+          this.resultado = (parseInt(this.calculadora[0]) * parseInt(this.calculadora[2])).toString();
+          break;
+        }
+        case '+': {
+          this.resultado = (parseInt(this.calculadora[0]) + parseInt(this.calculadora[2])).toString();
+          break;
+        }
+        case '-': {
+          this.resultado = (parseInt(this.calculadora[0]) - parseInt(this.calculadora[2])).toString();
+          break;
+        }
+       }
+
+       this.canWrite = false;
+       this.resetArray();
+      }
+  }
+
+  limpiar():void {
+    this.resultado = '';
+    this.canWrite = true;
+    this.resetArray();
+  }
+
+  resetArray():void {
+    this.calculadora.length = 0;
+  }
+
 }
